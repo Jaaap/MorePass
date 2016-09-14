ec129b1f5c8c88c9c6bfdc5b613b871803d1adfe
teijsden <teijsden@betterbe.com>
2016-09-14 13:29:02 +0200

    Added initial crypto.js

--- background.js
+++ background.js
@@ -1,77 +1,79 @@
-(function(){
-
-function Vault()
+//import * as crypto from "crypto";
 {
-	let self = this;
-	this.vault = [];
-	chrome.storage.local.get("vault", function(result){
-		self.vault = result.vault||[];
-		console.log(999);
-	});
-	console.log(this.vault);
-}
+	'use strict';
 
-Vault.prototype = {
-	get: function() {
-		return this.vault;
-	},
-	add: function(siteset) {
-		this.vault.push(siteset);
-		this.vault.sort(function(aa,bb){
-			let a = aa[0][0].hostname;
-			let b = bb[0][0].hostname;
-			return (a < b ? -1 : +(a > b));
+	function Vault()
+	{
+		let self = this;
+		this.vault = [];
+		chrome.storage.local.get("vault", function(result){
+			self.vault = result.vault||[];
+			console.log(999);
 		});
-		return this.save();
-	},
-	edit: function(idx, siteset) {
-		this.vault[idx] = siteset;
-		return this.save();
-	},
-	del: function(idx) {
-		this.vault.splice(idx, 1);
-		return this.save();
-	},
-	save: function() {
-		chrome.storage.local.set({"vault": this.vault});
-		return this.vault;
+		console.log(this.vault);
 	}
-};
 
-let vaultObj = new Vault();
+	Vault.prototype = {
+		get: function() {
+			return this.vault;
+		},
+		add: function(siteset) {
+			this.vault.push(siteset);
+			this.vault.sort(function(aa,bb){
+				let a = aa[0][0].hostname;
+				let b = bb[0][0].hostname;
+				return (a < b ? -1 : +(a > b));
+			});
+			return this.save();
+		},
+		edit: function(idx, siteset) {
+			this.vault[idx] = siteset;
+			return this.save();
+		},
+		del: function(idx) {
+			this.vault.splice(idx, 1);
+			return this.save();
+		},
+		save: function() {
+			chrome.storage.local.set({"vault": this.vault});
+			return this.vault;
+		}
+	};
 
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-	if (sender.tab) //from content
-	{
-		if (request.action === "submit")
-		{
-			//chrome.browserAction.show(sender.tab.id);
-			chrome.browserAction.setBadgeText({text: "1"});
-			let username = request.username;
-			let password = request.password;
-			//FIXME: store these credentials somewhere for the popup to use.
-		}
-	}
-	else //from popup
-	{
-		if (request.action === "vault.get")
-		{
-			sendResponse(vaultObj.get());
-		}
-		else if (request.action === "vault.add")
-		{
-			sendResponse(vaultObj.add(request.siteset));
-		}
-		else if (request.action === "vault.edit")
-		{
-			sendResponse(vaultObj.edit(request.idx, request.siteset));
-		}
-	}
-});
+	let vaultObj = new Vault();
 
-chrome.browserAction.onClicked.addListener(function(){ console.log("browserAction.onClicked"); });
-chrome.runtime.onInstalled.addListener(function(){ console.log("runtime.onInstalled"); });
-chrome.runtime.onStartup.addListener(function(){ console.log("runtime.onStartup"); });
-chrome.tabs.onUpdated.addListener(function(){ console.log("tabs.onUpdated"); });
+	chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+		if (sender.tab) //from content
+		{
+			if (request.action === "submit")
+			{
+				//chrome.browserAction.show(sender.tab.id);
+				chrome.browserAction.setBadgeText({text: "1"});
+				let username = request.username;
+				let password = request.password;
+				//FIXME: store these credentials somewhere for the popup to use.
+			}
+		}
+		else //from popup
+		{
+			if (request.action === "vault.get")
+			{
+				sendResponse(vaultObj.get());
+			}
+			else if (request.action === "vault.add")
+			{
+				sendResponse(vaultObj.add(request.siteset));
+			}
+			else if (request.action === "vault.edit")
+			{
+				sendResponse(vaultObj.edit(request.idx, request.siteset));
+			}
+		}
+	});
 
-})();
+	chrome.browserAction.onClicked.addListener(function(){ console.log("browserAction.onClicked"); });
+	chrome.runtime.onInstalled.addListener(function(){ console.log("runtime.onInstalled"); });
+	chrome.runtime.onStartup.addListener(function(){ console.log("runtime.onStartup"); });
+	chrome.tabs.onUpdated.addListener(function(){ console.log("tabs.onUpdated"); });
+
+}
