c8c5756e34df352a5f4901a05264fcce5019b00f
Jaaap <teun@be.nl>
2017-03-09 11:09:04 +0100

    Trivial cleanup

--- utils.js
+++ utils.js
@@ -20,14 +20,14 @@ function parseCSV(strData, strDelimiter)
 	let objPattern = new RegExp("(\\" + strDelimiter + "|\\r?\\n|\\r|^)" + "(?:\"([^\"]*(?:\"\"[^\"]*)*)\"|" + "([^\"\\" + strDelimiter + "\\r\\n]*))", "gi");
 	let arrData = [[]];
 	let arrMatches = null;
-	while (arrMatches = objPattern.exec( strData ))
+	while (arrMatches = objPattern.exec(strData))
 	{
 		let strMatchedDelimiter = arrMatches[ 1 ];
 		if (strMatchedDelimiter.length && (strMatchedDelimiter != strDelimiter))
 			arrData.push([]);
 		let strMatchedValue;
 		if (arrMatches[2])
-			strMatchedValue = arrMatches[2].replace(new RegExp( "\"\"", "g" ), "\"");
+			strMatchedValue = arrMatches[2].replace(/""/g, "\"");
 		else
 			strMatchedValue = arrMatches[3];
 		arrData[arrData.length - 1].push(strMatchedValue);
